076469ceab30706f43ea3c35e3e9b573f660f248
Wing Ortiz <wing.ortiz_sd@hotmail.com>
2023-01-15 18:58:15 -0600

    Fixing bugs on createGroup method of class Subject

--- org_cuatri.js
+++ org_cuatri.js
@@ -24,30 +24,34 @@ class Student{
 class Quarter{
     constructor(subjects,number_quarter){
         this.subjects = subjects
+        this.number_quarter = number_quarter
     }
 }
 
 class Subject{
     static group = []
-    constructor(number_students,number_quarter){
+    constructor(subject_name,number_quarter){
         this.number_quarter = number_quarter
-        this.number_students = number_students
+        this.subject_name = subject_name;
     }
 
     createGroup(studentsList){
+        if(studentsList.length < 5){
+            return console.log("Not enough students to create group");
+        }
         for(let i=0;i<studentsList.length;i++){
-            if(studentsList.length < 5){
-                return console.log("Not enough students to create group");
-            }
             if(Subject.group.length === 8){
-                groupsList.push(group);
+                groupsList.push({"Subject" :this.subject_name, "Group":Subject.group});
                 Subject.group.length = 0;
-
+                
             }
-            Subject.group.push(studentsList[i])
+            if(studentsList[i].getStatus()){
+                Subject.group.push(studentsList[i])
+            }
         }
         if(Subject.group.length > 4 && Subject.group.length < 9  ){
-            groupsList.push(Subject.group);
+            groupsList.push({"Subject" :this.subject_name, "Group":Subject.group});
+            // groupsList.push(Subject.group);
         }else{
             return console.log("Not enough students to create group");
 
@@ -56,5 +60,19 @@ class Subject{
 }
 
 
-const groupsList = []
-const studentsList = []
+let groupsList = []
+let studentsList = [new Student("Manuel",true,2), new Student("Wing",true,3), new Student("Cesar",true,1),new Student("Wlie",true,1), new Student("José",true,1),new Student("Esteban",true,1), new Student("Sebastian",true,1)];
+
+
+let quarter1 = new Quarter([new Subject("Programación I",1),new Subject("Redes I",1)], 1);
+let quarter2 = new Quarter([new Subject("Programación Orientada a Objetos",2),new Subject("Estructura de Datos y Algorítmos",2)], 2);
+let quarter3 = new Quarter([new Subject("Ingeniería en Software I",3),new Subject("Cálculo Integral",3)], 3);
+
+quarter1.subjects[0].createGroup(studentsList.filter(student => student.getQuarter() === quarter1.number_quarter))
+console.log(studentsList.filter(student => student.getQuarter() === quarter1.number_quarter))
+
+
+console.log(JSON.stringify(groupsList))
+
+
+
